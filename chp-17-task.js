f4b34baa59488e925ba0e437a542f69ebf5e634b
Your Nameecho <you@example.com>
2024-04-22 21:18:28 +0530

    small changes

--- chp-17-task.js
+++ chp-17-task.js
@@ -4,66 +4,66 @@ let inputEl = document.querySelector('input');
 // Check length of input value
 allBtn[0].onclick = () => {
 
-    Swal.fire({
-        title: `Your Value Lenght is ${inputEl.value.length}`,
-        showClass: {
-            popup: `
+  Swal.fire({
+    title: `Your Value Lenght is ${inputEl.value.length}`,
+    showClass: {
+      popup: `
             animate__animated
             animate__fadeInUp
             animate__faster
           `
-        },
-        hideClass: {
-            popup: `
+    },
+    hideClass: {
+      popup: `
             animate__animated
             animate__fadeOutDown
             animate__faster
           `
-        }
-    });
+    }
+  });
 }
 
 // Check char
 allBtn[1].onclick = () => {
 
-    Swal.fire({
-        title: `Your Last Char is " ${inputEl.value.charAt(inputEl.value.length - 1)} "`,
-        showClass: {
-            popup: `
+  Swal.fire({
+    title: `Your Last Char is " ${inputEl.value.charAt(inputEl.value.length - 1)} "`,
+    showClass: {
+      popup: `
             animate__animated
             animate__fadeInUp
             animate__faster
           `
-        },
-        hideClass: {
-            popup: `
+    },
+    hideClass: {
+      popup: `
             animate__animated
             animate__fadeOutDown
             animate__faster
           `
-        }
-    });
+    }
+  });
 }
 
 // Check Match
 allBtn[2].onclick = () => {
-    let dataMatchWith = "Just For Code";
+  let dataMatchWith = "Just For Code";
 
-    Swal.fire({
-        title: dataMatchWith.match(inputEl.value) ? "YOUR DATA IS MATCHED WITH THE INPUT VALUE" : `Please input "Just For Code" to match the data.`,
-        showClass: {
-            popup: `
+  Swal.fire({
+    title: inputEl.value != "" && dataMatchWith.match(inputEl.value) ? "YOUR DATA IS MATCHED WITH THE INPUT VALUE" : `Please input "Just For Code" to match the data.`,
+    showClass: {
+      popup: `
             animate__animated
             animate__fadeInUp
             animate__faster
           `
-        },
-        hideClass: {
-            popup: `
+    },
+    hideClass: {
+      popup: `
             animate__animated
             animate__fadeOutDown
             animate__faster
           `
-        }
-    });
+    }
+  });
 }
